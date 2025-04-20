46796f78e5250c3aac03c5905f1f8f4612f251be
Dwi Aryati <agus.aryati@danamon.co.id>
2025-04-20 16:01:46 +0700

    add script config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const app = express();
 // const port = process.env.PORT || 5000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Use CORS middleware with specific origin (React frontend)
 app.use(cors());
